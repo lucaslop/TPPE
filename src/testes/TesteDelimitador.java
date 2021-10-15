8e94bc966057fe25aaf42b31afb8907056e2130d
lucas lopes <lucaslopes05597@gmail.com>
2021-10-15 18:49:23 -0300

    [DUPLICAÇÃO] teste definir duplicador + teste de exceção
    Co-Authored-By: Luis Bruno <l.brunofidelis@gmail.com>

--- src/testes/TesteDelimitador.java
+++ src/testes/TesteDelimitador.java
@@ -1,5 +1,9 @@
 package testes;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import aplication.Parser;
 import exceptions.DelimitadorException;
 public class TesteDelimitador {
@@ -7,8 +11,25 @@ public class TesteDelimitador {
 	
 	Parser parser = new Parser();
 	
+	@Test
 	public void testeDelimitadorCampo() throws DelimitadorException{
-		parser.setDelimitador(';');
+		parser.setDelimitador(";");
 		assertEquals(';',parser.getDelimitador());
+	}
+	@Test
+	public void teste02DelimitadorCampo() throws DelimitadorException{
+		parser.setDelimitador("/");
+		assertEquals('/',parser.getDelimitador());
+	}
+	
+	@Test
+	public void teste03DelimitadorCampo() throws DelimitadorException{
+		parser.setDelimitador(",");
+		assertEquals(',',parser.getDelimitador());
+	}
+	
+	@Test(expected=DelimitadorException.class)
+	public void testDelimitadorInvalidoException() throws DelimitadorException {
+		parser.setDelimitador("string");
 	}
 }
