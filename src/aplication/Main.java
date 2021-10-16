fafaa111c6c4c0c072bc47bb7ce7aa098e649301
lucas lopes <lucaslopes05597@gmail.com>
2021-10-16 17:55:05 -0300

    Arquivo principal (main)
    Co-Authored-By: Luis Bruno <l.brunofidelis@gmail.com>

--- src/aplication/Main.java
+++ src/aplication/Main.java
@@ -2,19 +2,87 @@
  * 
  */
 package aplication;
+import java.util.Scanner;
+
+import exceptions.*;
 
 /**
  * @author lucas
  *
  */
-public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+	public class Main {
+
+		public static void main(String[] args) {
+			Parser parser = new Parser();
+			Scanner scanner = new Scanner(System.in);
+
+			System.out.print("Digite a pasta do arquivo de entrada: ");
+			String caminhoArquivoEntrada = scanner.next();
+			
+			System.out.print("Digite o tipo de dados do arquivo de entrada (1 - Inteiro, 2 - Double)");
+			int tipoArquivo = scanner.nextInt();
+			scanner.nextLine();
+
+			try {
+				if(tipoArquivo == 1)
+					parser.lerArq(caminhoArquivoEntrada,"inteiro");
+				if (tipoArquivo==2)
+					parser.lerArq(caminhoArquivoEntrada, "double");
+			} catch (ArquivoNaoEncontrado e) {
+				System.out.println(e);
+				System.exit(-1);
+			}
+
+			System.out.print("Digite o delimitador: ");
+			String delimitador = scanner.nextLine();
+
+			try {
+				parser.setDelimitador(delimitador);
+			} catch (DelimitadorException e) {
+				System.out.println(e);
+				System.exit(-1);
+			}
+
+			System.out.print("Digite o formato do arquivo de saida (0 - Linha, 1 - Coluna): ");
+			int formatoArquivoSaida = scanner.nextInt();
+			scanner.nextLine();
+
+			if (formatoArquivoSaida == 0 || formatoArquivoSaida == 1) {
+				parser.setFormatoSaida(formatoArquivoSaida);	
+			} else {
+				System.out.println("Formato de saída inválido.");
+				System.exit(-1);
+			}
+
+			System.out.print("Digite o caminho da pasta para salvar o arquivo de saída: ");
+			String caminhoArquivoSaida = scanner.nextLine();
+
+			try {
+				parser.setArquivoSaida(caminhoArquivoSaida);
+			} catch (EscritaNaoPermitidaException e) {
+				System.out.println(e);
+				System.exit(-1);
+			}
+
+			try {
+				if(tipoArquivo == 1)
+					parser.escreverArquivoDeResposta("inteiro");
+				if(tipoArquivo == 2)
+					parser.escreverArquivoDeResposta("double");
+			} catch (EscritaNaoPermitidaException e) {
+				System.out.println(e);
+				System.exit(-1);
+			}
+
+			System.out.println("Arquivo salvo na pasta " + caminhoArquivoSaida );
+			
+
+			scanner.close();
+		}
 
 	}
 
-}
+
