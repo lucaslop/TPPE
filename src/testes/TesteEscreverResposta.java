9026f90c98308fcfb396fd891d738d1fa9612c10
lucas lopes <lucaslopes05597@gmail.com>
2021-10-16 16:54:58 -0300

    [TRIANGULAÇÃO] triangualação dos testes p/ escrever o arquivo
    Co-Authored-By: Luis Bruno <l.brunofidelis@gmail.com>

--- src/testes/TesteEscreverResposta.java
+++ src/testes/TesteEscreverResposta.java
@@ -69,8 +69,67 @@ public class TesteEscreverResposta {
 		assertEquals(array, parser.getBufferDouble());
 	}
 	
+	@Test
+	public void teste02EscreverArquivoRespostaInteiroColuna() throws ArquivoNaoEncontrado, DelimitadorException, FileNotFoundException, EscritaNaoPermitidaException {
+		parser.lerArq("arquivos/analysisTime.out","inteiro");
+		parser.setDelimitador("/");
+		parser.setFormatoSaida(Parser.coluna);
+		parser.setArquivoSaida("arquivos");
+		parser.escreverArquivoDeResposta("inteiro");
 
+		String path = parser.getArquivoDeResposta();
+		Scanner input = new Scanner(new FileReader(path));
+		Vector <Vector <Integer>> array = new Vector <Vector <Integer>>();
+		int numero_linhas=0;
+		while(input.hasNextLine()) {
+			String dados = input.nextLine();
+			String colunas[] = dados.split("/");
+			if(numero_linhas == 0) {
+				for(String each:colunas) {
+					array.add(new Vector<Integer>());
+				}
+			}else {
+				for(int i=0; i<colunas.length; i++) {
+					array.elementAt(i).add(Integer.valueOf(colunas[i]));
+				}
+			}
 
+			numero_linhas++;
+		}
+
+		assertEquals(array, parser.getBufferInteger());
+	}
+	
+	@Test
+	public void teste03EscreverArquivoRespostaDoubleColuna() throws ArquivoNaoEncontrado, DelimitadorException, FileNotFoundException, EscritaNaoPermitidaException {
+		parser.lerArq("arquivos/analysisMemory.out","double");
+		parser.setDelimitador("/");
+		parser.setFormatoSaida(Parser.coluna);
+		parser.setArquivoSaida("arquivos");
+		parser.escreverArquivoDeResposta("double");
+
+		String path = parser.getArquivoDeResposta();
+		Scanner input = new Scanner(new FileReader(path));
+		Vector <Vector <Double>> array = new Vector <Vector <Double>>();
+		int numero_linhas=0;
+		while(input.hasNextLine()) {
+			String dados = input.nextLine();
+			String colunas[] = dados.split("/");
+			if(numero_linhas == 0) {
+				for(String each:colunas) {
+					array.add(new Vector<Double>());
+				}
+			}else {
+				for(int i=0; i<colunas.length; i++) {
+					array.elementAt(i).add(Double.valueOf(colunas[i]));
+				}
+			}
+
+			numero_linhas++;
+		}
+
+		assertEquals(array, parser.getBufferDouble());
+	}
 }
 
 
